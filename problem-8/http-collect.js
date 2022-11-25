34d5d3cb1caa99953907c178bb486a609ad2a9e4
ddoyediran <ddoyediran@gmail.com>
2022-11-25 01:34:35 +0100

    problem 9 solved

--- problem-8/http-collect.js
+++ problem-8/http-collect.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const path = require("path");
+//const path = require("path");
 const http = require("http");
 const bl = require("bl");
 
---
+++ problem-9/juggling-async.js
@@ -0,0 +1,32 @@
+// const fs = require("fs");
+const http = require("http");
+const bl = require("bl");
+
+const pathLength = process.argv.slice(2);
+let count = pathLength.length;
+
+let result = [];
+
+for (let i = 0; i < pathLength.length; i++) {
+  http.get(pathLength[i], function (response) {
+    response.pipe(
+      bl((err, data) => {
+        if (err) {
+          return console.error(err);
+        }
+
+        result[i] = data.toString();
+        count = count - 1;
+
+        if (count === 0) {
+          for (let i = 0; i < result.length; i++) {
+            console.log(result[i]);
+          }
+        }
+        //result.push(data.toString());
+        //console.log(data.length); // length of the data
+        //console.log(data.toString()); // print the data
+      })
+    );
+  });
+}
